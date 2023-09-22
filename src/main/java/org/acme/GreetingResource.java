3c3d19be6f94c62125a3ca85c529fe051d477fe7
lokeshrangineni <lokeshforjava@gmail.com>
2023-09-22 14:01:38 -0400

    changing so that CI/CD pipeline triggers.
    // changing so that CI/CD pipeline triggers.

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+// changing so that CI/CD pipeline triggers.
 @Path("/hello-resteasy")
 public class GreetingResource {
 
